15181e630279378ca2687463d5b31b15f23317e9
Josh Ralph <josh.ralph@netmatters.com>
2016-07-27 18:16:33 +0100

    added rule classes and tests

--- src/PasswordPolicy/Policy.php
+++ src/PasswordPolicy/Policy.php
@@ -4,66 +4,8 @@ class Policy
 {
     protected $rules = [];
 
-    public function minLength($length)
+    public function addRule(Rule $rule)
     {
-        return $this->pattern(sprintf('.{%d,}', $length));
+        $this->rules[] = $rule;
     }
-
-    public function doesNotContain($phrases)
-    {
-        $phrases = is_array($phrases) ? $phrases : func_get_args();
-
-        return $this->closure(function ($subject) use ($phrases) {
-            foreach ($phrases as $phrase) {
-                if (strpos($subject, $phrase) !== false) {
-                    return false;
-                }
-            }
-
-            return true;
-        });
-    }
-
-    public function upperCase($min = 1)
-    {
-        return $this->patternMin(sprintf('[A-Z]'), $min);
-    }
-
-    public function lowerCase($min = 1)
-    {
-        return $this->patternMin(sprintf('[a-z]'), $min);
-    }
-
-    public function digits($min = 1)
-    {
-        return $this->patternMin(sprintf('[0-9]'), $min);
-    }
-
-    public function pattern($pattern, $flags = '')
-    {
-        return $this->closure(function ($subject) use ($pattern, $flags) {
-            return preg_match("/{$pattern}/{$flags}", $subject);
-        });
-    }
-
-    public function patternMin($pattern, $min = 1, $flags = '')
-    {
-        return $this->closure(function ($subject) use ($pattern, $min, $flags) {
-            return preg_match_all("/{$pattern}/{$flags}", $subject) >= $min;
-        });
-    }
-
-    public function closure(\Closure $closure)
-    {
-        $this->rules[] = $closure;
-
-        return $this;
-    }
-
-    public function rules()
-    {
-        return $this->rules;
-    }
-
-
 }
---
+++ src/PasswordPolicy/PolicyBuilder.php
@@ -0,0 +1,113 @@
+<?php namespace PasswordPolicy;
+
+use PasswordPolicy\Rules\CaseRule;
+use PasswordPolicy\Rules\ContainRule;
+use PasswordPolicy\Rules\DigitRule;
+use PasswordPolicy\Rules\LengthRule;
+
+/**
+ * Class PolicyBuilder
+ *
+ * @package PasswordPolicy
+ */
+class PolicyBuilder
+{
+    /**
+     * Policy instance
+     *
+     * @var Policy
+     */
+    private $policy;
+
+
+    /**
+     * PolicyBuilder constructor.
+     *
+     * @param Policy $policy
+     */
+    public function __construct(Policy $policy)
+    {
+        $this->policy = $policy;
+    }
+
+    /**
+     * Add a min length rule
+     *
+     * @param $length int
+     *
+     * @return $this
+     */
+    public function minLength($length)
+    {
+        $this->policy->addRule(
+            (new LengthRule)->min($length)
+        );
+
+        return $this;
+    }
+
+    /**
+     * Add a max length rule
+     *
+     * @param $length int
+     *
+     * @return $this
+     */
+    public function maxLength($length)
+    {
+        $this->policy->addRule(
+            (new LengthRule)->max($length)
+        );
+
+        return $this;
+    }
+
+    public function contains($phrases)
+    {
+        $phrases = is_array($phrases) ? $phrases : func_get_args();
+
+        $this->policy->addRule(
+            (new ContainRule)->phrase($phrases)
+        );
+
+        return $this;
+    }
+
+    public function doesNotContain($phrases)
+    {
+        $phrases = is_array($phrases) ? $phrases : func_get_args();
+
+        $this->policy->addRule(
+            (new ContainRule)->phrase($phrases)->doesnt()
+        );
+
+        return $this;
+    }
+
+    public function upperCase($min = 1)
+    {
+        $this->policy->addRule(
+            (new CaseRule)->upper($min)
+        );
+
+        return $this;
+    }
+
+    public function lowerCase($min = 1)
+    {
+        $this->policy->addRule(
+            (new CaseRule)->lower($min)
+        );
+
+        return $this;
+    }
+
+    public function digits($min = 1)
+    {
+        $this->policy->addRule(
+            (new DigitRule)->min($min)
+        );
+
+        return $this;
+    }
+}
